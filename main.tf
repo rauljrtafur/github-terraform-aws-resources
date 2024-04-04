1f9790dc2296cc6137ab94e207b44e65f62c9e27
rauljrtafur95 <raul.tafur@rappi.com>
2024-04-04 11:07:55 -0500

    rename bucket

--- main.tf
+++ main.tf
@@ -16,7 +16,7 @@ provider "aws" {
 module "s3_bucket" {
   source = "terraform-aws-modules/s3-bucket/aws"
 
-  bucket = "imageGenerate"
+  bucket = "image-generate"
   acl    = "private"
 
   control_object_ownership = true
